8feb273d8585b745f1dad63cb5285b733c676cfd
abhishekkr1818 <abhishek.kumar.iotcs27@heritageit.edu.in>
2025-07-11 01:37:08 +0530

    Zod validation

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -6,33 +6,103 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { slugify } from "slugmaster";
 import ImageUpload from "./ImageUpload";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { z } from "zod";
 
-export default function Editor({onSave,initialData}) {
-  const { register, handleSubmit,setValue } = useForm();
+const schema = z.object({
+  title: z
+    .string()
+    .min(10, { message: "Title must contain 10 or more characters" })
+    .min(1, { message: "Title must not be empty" }),
+  excerpt: z
+    .string()
+    .min(10, { message: "Please add some details in the excerpt" }),
+  category: z.string().min(1, { message: "Please add a category" }),
+  metaDescription: z.string().optional(),
+  keywords: z
+    .string()
+    .min(1, { message: "Keywords should be there for SEO benefits" }),
+  status: z.enum(["DRAFT", "PUBLISHED"]),
+});
+
+export default function Editor({ onSave, initialData }) {
+  const { register, handleSubmit, setValue } = useForm();
   const [content, setContent] = useState("");
   const [ogImage, setOgImage] = useState("");
+  const router = useRouter();
 
-  useEffect(()=> {
-    if(initialData){
-      setValue('title', initialData.title);
+  useEffect(() => {
+    if (initialData) {
+      setValue("title", initialData.title);
       setContent(initialData.content);
-      setOgImage(initialData.thumbnail)
-      setValue('keywords', initialData.keywords || "");
-      setValue('category', initialData.catSlug || "");
-      setValue('excerpt', initialData.excerpt || "");
-      setValue('metaDescription', initialData.desc || "");
-      setValue('status', initialData.status);
+      setOgImage(initialData.thumbnail);
+      setValue("keywords", initialData.keywords || "");
+      setValue("category", initialData.catSlug || "");
+      setValue("excerpt", initialData.excerpt || "");
+      setValue("metaDescription", initialData.desc || "");
+      setValue("status", initialData.status);
     }
-  }, [initialData])
+  }, [initialData]);
 
   const handleForm = (data) => {
-    console.log(data);
-    const generatedSlug = initialData ? initialData.slug : slugify(data.title);
-    onSave({...data, slug: generatedSlug,ogImage, content});
+    try {
+      const generatedSlug = initialData
+        ? initialData.slug
+        : slugify(data.title);
+      onSave({ ...data, slug: generatedSlug, ogImage, content });
+      toast({
+        title: "Success",
+        description: initialData
+          ? "Your blog was updated"
+          : "Your blog page was published",
+      });
+      if (data.status === "PUBLISHED") router.push(`/blog/${generatedSlug}`);
+    } catch (error) {
+      console.error(error.message);
+    }
   };
+
   return (
     <section>
-      <form className="space-y-4" onSubmit={handleSubmit(handleForm)}>
+      <form
+        className="space-y-4"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await schema.parseAsync(data);
+            await handleForm(data);
+          } catch (error) {
+            // catch (error) {
+            //   console.error(error.message);
+            //   if(error instanceof z.ZodError){
+            //     error.errors.forEach(error=> {
+            //       toast({ title: "Error", description: error.message, variant: "destructive"})
+            //     })
+            //   }
+            // }
+
+            // Safe log
+            console.error("Validation failed", error);
+
+            if (error instanceof z.ZodError) {
+              error.issues.forEach((err) => {
+                toast({
+                  title: "Validation Error",
+                  description: err.message,
+                  variant: "destructive",
+                });
+              });
+            } else {
+              toast({
+                title: "Something went wrong",
+                description: error?.message || "Unknown error",
+                variant: "destructive",
+              });
+            }
+          }
+        })}
+      >
+      
         <input
           {...register("title")}
           placeholder="Enter an title"
@@ -93,19 +163,19 @@ export default function Editor({onSave,initialData}) {
           type="text"
         />
         <div className="flex gap-2">
-            <select
-                {...register("status")}
-                className="font-bold text-lg bg-zinc-600 px-3 py-1 rounded-sm outline-none"
-            >
-                <option value="DRAFT">Draft</option>
-                <option value="PUBLISHED">Publish</option>
-            </select>
-            <button
-                type="submit"
-                className="bg-zinc-800 px-3 py-2 rounded cursor-pointer"
-                >
-                Save
-            </button>
+          <select
+            {...register("status")}
+            className="font-bold text-lg bg-zinc-600 px-3 py-1 rounded-sm outline-none"
+          >
+            <option value="DRAFT">Draft</option>
+            <option value="PUBLISHED">Publish</option>
+          </select>
+          <button
+            type="submit"
+            className="bg-zinc-800 px-3 py-2 rounded cursor-pointer"
+          >
+            Save
+          </button>
         </div>
       </form>
     </section>
